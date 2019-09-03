7c0e2d64226b62918f083325b2a7a42b3abec8bd
joris.caze <joris.caze@gmail.com>
2019-09-03 08:58:00 +0200

    Script to compress pdf

--- Shell/pdfComp.sh
+++ Shell/pdfComp.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 # Compress a pdf file with GhostScript
+# How to use : ./sPdfComp.sh exampleFile.pdf
 
-if [-n $#]; then
+if [ -z $1 ]; then
+	echo '-> Error : no input file'
+elif [ ! -e $1 ]; then
+	echo '-> Error : file not found'
+else
 	echo '-> Compressing...'
 	gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=out.pdf $1
 	echo "-> Compressed file $1 done"
-else
-	echo '-> Error : no input file'
 fi
 
